bd800dff8b54cde3b3cce14dea8981bdc7b7994c
Vladimir <nikonoshin1999@gmail.com>
2026-03-18 03:43:38 +0400

    Update ANIMATOR.sh

--- ANIMATOR.sh
+++ ANIMATOR.sh
@@ -169,6 +169,12 @@ provisioning_start
 
 rm -f /.provisioning
 
+cd "${COMFYUI_DIR}"
+export TORCH_CUDNN_V8_API_ENABLED=0
+
+echo ">>> Освобождаем порт 8188..."
+fuser -k 8188/tcp || true
+
 # --- ФИНАЛЬНЫЙ БАННЕР HERWAM ---
 echo "##############################################################"
 echo "#                                                            #"
@@ -197,11 +203,5 @@ echo "#   КОПИРОВАНИЕ ИЛИ ИСПОЛЬЗОВАНИЕ БЕЗ РАЗ
 echo "#   ДЛЯ СОТРУДНИЧЕСТВА ОБРАЩАТЬСЯ В TG https://t.me/vnknshn                     #"
 echo "#################################################################################"
 echo " "
-
-cd "${COMFYUI_DIR}"
-export TORCH_CUDNN_V8_API_ENABLED=0
-
-echo ">>> Освобождаем порт 8188..."
-fuser -k 8188/tcp || true
 
 $VENV_PYTHON main.py --listen 0.0.0.0 --port 8188 --highvram --use-pytorch-cross-attention
