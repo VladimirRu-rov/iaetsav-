6ea86afc752bb1312f5cd8750d05c507efc8568f
Vladimir <nikonoshin1999@gmail.com>
2026-03-18 02:24:48 +0400

    Update CONTROL_NET.sh

--- CONTROL_NET.sh
+++ CONTROL_NET.sh
@@ -54,6 +54,7 @@ NODES=(
     "https://github.com/teskor-hub/comfyui-teskors-utils"
     "https://github.com/hanjangma41/NEW-UTILSs"
     "https://github.com/WASasquatch/was-node-suite-comfyui"
+    "https://github.com/ubilabs/node-image-saver.git"
 )
 # --- ФУНКЦИИ ---
 
