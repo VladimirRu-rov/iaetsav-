612171f37c60d638924d67a73fadc9bfe58ae688
Vladimir <nikonoshin1999@gmail.com>
2026-03-18 04:20:07 +0400

    Update NSFW.sh

--- NSFW.sh
+++ NSFW.sh
@@ -186,12 +186,6 @@ provisioning_start
 
 rm -f /.provisioning
 
-cd "${COMFYUI_DIR}"
-export TORCH_CUDNN_V8_API_ENABLED=0
-
-echo ">>> Очистка портов..."
-fuser -k 8188/tcp || true
-
 echo "=== ЕБАШИМ ДЕНЬГИ запускает ComfyUI ==="
 # --- ФИНАЛЬНЫЙ БАННЕР HERWAM ---
 echo "##############################################################"
@@ -221,5 +215,3 @@ echo "#   КОПИРОВАНИЕ ИЛИ ИСПОЛЬЗОВАНИЕ БЕЗ РАЗ
 echo "#   ДЛЯ СОТРУДНИЧЕСТВА ОБРАЩАТЬСЯ В TG https://t.me/vnknshn                     #"
 echo "#################################################################################"
 echo " "
-
-$VENV_PYTHON main.py --listen 0.0.0.0 --port 8188 --highvram --use-pytorch-cross-attention
